16bf73bbc1370c1d89b810d6b73c8b7a2aba58a9
Madhumitha P Ravishankar <madhumithap.ravishankar@gmail.com>
2019-11-10 12:40:07 +0530

    Update api request over https

--- moviewiki/main.js
+++ moviewiki/main.js
@@ -16,18 +16,18 @@ btn.addEventListener('click', function (event) {
 async function find() {
     console.log('search');
     let keyword = document.getElementById('search').value;
-    let res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=813bdc23&s=${keyword}&page=${page}`);
+    let res = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=813bdc23&s=${keyword}&page=${page}`);
     let data = await res.json();
     let result = document.getElementById('result');
     result.innerHTML = createResult(data);
-    moreBtn.style.display = "block"
+    moreBtn.style.display = "block";
 }
 
 function createResult(data) {
     let card = " ";
     console.log(data);
     data.Search.forEach((movie) => {
-        card += `<a href="/moviewiki/movie.html?${movie.imdbID}"> <div class="card" id="${movie.imdbID}">
+        card += `<a href="/movie.html?${movie.imdbID}"> <div class="card" id="${movie.imdbID}">
         <img src="${movie.Poster}" id="${movie.imdbID}" alt="${movie.Title}">
         <div class="container center">
           <p>${movie.Title}</p>
@@ -45,7 +45,7 @@ moreBtn.addEventListener('click', function (event) {
 async function showMore(){
     let keyword = document.getElementById('search').value;
     page += 1;
-    let res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=813bdc23&s=${keyword}&page=${page}`);
+    let res = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=813bdc23&s=${keyword}&page=${page}`);
     let data = await res.json();
     let result = document.getElementById('result');
     result.innerHTML += createResult(data);
